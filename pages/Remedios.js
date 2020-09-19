d8882229e3cedd98341a03285e2c4cbf9e8b9e0a
cherryramatis <fah.ramatis@gmail.com>
2020-09-19 17:55:18 -0300

    tela remedios finalizada

--- pages/Remedios.js
+++ pages/Remedios.js
@@ -1,45 +1,91 @@
-import React from "react"
-import { View, TouchableOpacity, Text, StyleSheet } from 'react-native'
-import { AntDesign } from '@expo/vector-icons';
+import React from "react";
+import {
+  View,
+  TouchableOpacity,
+  Text,
+  StyleSheet,
+  TextInput,
+} from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+import { StatusBar } from "expo-status-bar";
 
-function Remedios(){
-    return(
-        <View style={styles.caixa}>
-            <Text style={styles.link}>Remedios</Text>
-
-            <TouchableOpacity style={styles.botao}>
-            <Text style={styles.texto}>Calendário</Text>
-            </TouchableOpacity>
-
-            <Text style={[styles.novo, { fontSize: '10'}]}>Nome do Medicamento:</Text>
-
-            <Text style={styles.novo}>Horário</Text>
-
-            <AntDesign name="pluscircleo" size={24} color="#FEF745" />
-
-            <View style={styles.botao_voltar_caixa}>
-                <TouchableOpacity style={styles.botao_voltar}>
-                <Text style={styles.botao_voltar_texto}>Voltar</Text>
-                </TouchableOpacity>
-            </View>
+function Remedios() {
+  return (
+    <>
+      <StatusBar style="dark" />
+      <View style={styles.caixa}>
+        <View style={styles.tituloContainer}>
+          <Text style={styles.titulo}>Remedios</Text>
         </View>
-        );
-} 
-const styles = StyleSheet.create({
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "space-evenly",
+            marginTop: 100,
+            flex: 1,
+          }}
+        >
+          <TouchableOpacity style={styles.botao}>
+            <Text style={styles.texto}>Calendário</Text>
+          </TouchableOpacity>
+          <Text style={styles.novo}>Medicamento:</Text>
+          <TextInput placeholder="" style={styles.input} />
+          <Text style={styles.novo}>Horário:</Text>
+          <TextInput placeholder="" style={styles.inputMenor} />
 
-    texto: {
-    fontSize: 30,
+          <AntDesign name="pluscircleo" size={50} color="#FEF745" />
+
+          <View style={styles.footer}>
+            <View style={styles.botao_voltar_caixa}>
+              <TouchableOpacity style={styles.botao_voltar}>
+                <Text style={styles.botao_voltar_texto}>Voltar</Text>
+              </TouchableOpacity>
+            </View>
+            <Text style={styles.badge}>SENIORS</Text>
+          </View>
+        </View>
+      </View>
+    </>
+  );
+}
+const styles = StyleSheet.create({
+  texto: {
+    fontSize: 25,
     color: "#FEF745",
     fontWeight: "bold",
     textTransform: "uppercase",
-    textDecorationLine:"underline"
+    textDecorationLine: "underline",
   },
 
-  novo: { 
-      fontSize: 30,
-      color: "#FFFFFF",
-      textTransform:"uppercase",
-      fontWeight: "bold",
+  titulo: {
+    color: "#000",
+    fontWeight: "bold",
+    fontSize: 47,
+    textTransform: "uppercase",
+  },
+
+  tituloContainer: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#FEF745",
+    width: "100%",
+    height: 90,
+    position: "absolute",
+    top: 0,
+    paddingTop: 20,
+    paddingLeft: 10,
+    borderBottomLeftRadius: 50,
+    borderBottomRightRadius: 50,
+  },
+
+  novo: {
+    fontSize: 30,
+    color: "#FFFFFF",
+    textTransform: "uppercase",
+    fontWeight: "bold",
   },
 
   link: {
@@ -53,6 +99,26 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "space-around",
     backgroundColor: "#5A60FE",
+  },
+
+  input: {
+    width: 315,
+    height: 54,
+    borderRadius: 50,
+    borderWidth: 5,
+    borderColor: "#FEF745",
+    padding: 10,
+    backgroundColor: "#fff",
+  },
+
+  inputMenor: {
+    width: 202,
+    height: 54,
+    borderRadius: 50,
+    borderWidth: 5,
+    borderColor: "#FEF745",
+    padding: 10,
+    backgroundColor: "#fff",
   },
 
   botao_voltar: {
@@ -73,6 +139,20 @@ const styles = StyleSheet.create({
 
     elevation: 14,
   },
+
+  footer: {
+    flexDirection: "row",
+    alignItems: "center",
+    width: "70%",
+  },
+
+  badge: {
+    fontSize: 25,
+    color: "#FEF745",
+    fontWeight: "bold",
+    textTransform: "uppercase",
+  },
+
   botao_voltar_texto: {
     fontSize: 25,
     color: "#000000",
@@ -86,4 +166,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default Remedios
+export default Remedios;
